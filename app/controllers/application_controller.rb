edc9546a47013d42dc910c60d9afac66f7d4650b
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-07-08 14:42:55 +0000

    [Update] devise_install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # 上の文章を記述することで、devise機能(登録やログイン認証)
+  # が使われる前に下のparametersメソッドが実行される設定
+
+  # 呼び出せれた他のControllerからも参照できるようにするため
+  # protectedを記述すること！
+  protected
+
+  # ストロングパラメーターはprivate⇒ほかのControllerから参照負荷
+  # deviseを使用⇒protectedをセットで使うこと！
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sing_up, keys: [:name])
+  end
+
+  #configure_permitted_parametersメソッドというのがある
+  #ユーザーの登録(sign_up)の際に、今回であればユーザー名(name)のデータ操作を許可する
+
+
+  #deviseコントローラーは修正できないので、追記はApplication_controllerにすること！
+
 end
